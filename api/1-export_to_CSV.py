ebcb3bb36b6b2f6bd607f90854de14d86d54a48f
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-16 14:41:25 -0600

    corrected my CSV connection

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -16,46 +16,25 @@ if __name__ == "__main__":
     if response_user.status_code == 200:
         user_data = json.loads(response_user.text)
         EMPLOYEE_ID = user_data[0]["id"]
-        EMPLOYEE_NAME = (user_data[0]["name"])
+        EMPLOYEE_NAME = (user_data[0]["username"])
 
     """ Fetching todo list based on ID """
     response_todos = requests.get(todos_url, {"userId": argv[1]})
     if response_todos.status_code == 200:
-        todo_data = json.loads(response_todos.text)
+        todo_data_list = json.loads(response_todos.text)
 
-    """ Initializing counters """
-    tasks = 0
-    completed_tasks = 0
-    list_of_tasks = []
+    BIG_task_list = []
 
-    """ Count completed tasks & create a list of their titles """
-    for item in todo_data:
-        if item["completed"]:
-            completed_tasks += 1
-            list_of_tasks.append(item["title"])
-        tasks += 1
+    for todo_data in todo_data_list:
+        completed = todo_data["completed"]
+        title = todo_data["title"]
+        new_list = [EMPLOYEE_ID, EMPLOYEE_NAME, completed, title]
+        BIG_task_list.append(new_list)
 
-    """ Generate & print to output_format """
-    output_format = "Employee {} is done with tasks({}/{}):".format(
-        EMPLOYEE_NAME, completed_tasks, tasks
-    )
-
-    print(output_format)
-
-    """ Printing titles of completed tasks with proper indentation """
-    for item in list_of_tasks:
-        print("\t {}".format(item))
-
-    """ Exporting data to CSV """
     csv_file_name = "{}.csv".format(EMPLOYEE_ID)
+
     with open(csv_file_name, mode="w", newline="") as csv_file:
-        csv_writer = csv.writer(csv_file)
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
-        """ Writing CSV header """
-        csv_writer.writerow(
-            ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-        """ Writing task details """
-        for task_title in list_of_tasks:
-            csv_writer.writerow(
-                [EMPLOYEE_ID, EMPLOYEE_NAME, "Completed", task_title])
+        for item in BIG_task_list:
+            csv_writer.writerow(item)
