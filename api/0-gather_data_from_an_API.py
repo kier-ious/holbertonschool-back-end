7caa782b69c12d5efd2aff3e1b5671f9447215d5
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-16 13:03:14 -0600

    corrected printing format

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,27 +3,34 @@
 import json
 import requests
 from sys import argv
+
 url = "https://jsonplaceholder.typicode.com/users/"
 todos_url = "https://jsonplaceholder.typicode.com/todos/"
 
 response = requests.get(url, {"id": argv[1]})
 if response.status_code == 200:
-    var = json.loads(response.text)
-    EMPLOYEE_NAME = (var[0]["name"])
+    user_data = json.loads(response.text)
+    EMPLOYEE_NAME = (user_data[0]["name"])
 
-response = requests.get(todos_url, {"userId": 1})
+response = requests.get(todos_url, {"userId": argv[1]})
 if response.status_code == 200:
-    var = json.loads(response.text)
-    # NUMBER_OF_DONE_TASKS = (var[0]["completed"])
-    print(var)
+    todo_data = json.loads(response.text)
 
 tasks = 0
 completed_tasks = 0
-list_of_tasks = [0]
+list_of_tasks = []
 
-for item in var:
+for item in todo_data:
     if item["completed"]:
         completed_tasks += 1
-        list_of_tasks.append(item)
+        list_of_tasks.append(item["title"])
     tasks += 1
-print(completed_tasks)
+
+output_format = "Employee {} is done with tasks({}/{}):".format(
+    EMPLOYEE_NAME, completed_tasks, tasks
+)
+
+print(output_format)
+
+for item in list_of_tasks:
+    print("\t {}".format(item))
